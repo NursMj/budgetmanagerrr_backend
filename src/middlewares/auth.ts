5059ad1c15aa78940be600c8a131eefea927e11f
Nursultan Mambetzhanov <nmambetzhanov@innovationdigital.kg>
2024-11-21 15:17:57 +0600

    feat: created auth middleware to protect routes

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,38 @@
+import Token from '@/models/Token';
+import User from '@/models/User';
+import { isEmpty } from '@/utils';
+
+import type { NextFunction, Request, Response } from 'express';
+
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const bearerHeader = req.headers['authorization'];
+
+		if (isEmpty(bearerHeader)) throw new Error('An unexpected error has occurred');
+
+		const access_token = bearerHeader?.split(/\s/)[1];
+		const user_token = await Token.query().findOne({
+			access_token,
+		});
+
+		if (!user_token) throw new Error('An unexpected error has occurred');
+
+		const user = await User.query().findOne({
+			id: user_token.user_id,
+		});
+
+		if (!user) {
+			throw new Error('An unexpected error has occurred');
+		}
+
+		next();
+	} catch (e: any) {
+		res
+			.status(401)
+			.json({
+				success: false,
+				message: 'Unauthorized',
+			})
+			.end();
+	}
+};
