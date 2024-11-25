2b19c9fefc85065b574dd8ab938ed38f2a91ea98
Nursultan Mambetzhanov <nmambetzhanov@innovationdigital.kg>
2024-11-25 09:23:19 +0600

    feat: changed category validation

--- src/validations/categoryValidationSchemas.ts
+++ src/validations/categoryValidationSchemas.ts
@@ -3,8 +3,5 @@ export const createCategoryValidationSchema = {
         notEmpty: {
             errorMessage: "Name cannot be empty"
         },
-        isString: {
-            errorMessage: "Name must be a string"
-        },
     },
 }
